c22eb0d8aa3bd92eb3fa171dbe73af5ea207cc37
tiburonchan3 <tiburonchan3@gmail.com>
2021-10-27 16:07:48 -0600

    pagination is added

--- src/Controllers/users.controller.js
+++ src/Controllers/users.controller.js
@@ -1,37 +1,74 @@
 const mysqlconnection = require("../Databases/Db");
+const Promise = require("bluebird");
 
 const saveUser = (req, res) => {
   const { Nombre, Apellido, Email, Clave } = req.body;
   const sql =
     "INSERT INTO usuarios(Nombre,Apellido,Email,Clave) values(?,?,?,?)";
-  mysqlconnection.query(
-    sql,
-    [Nombre, Apellido, Email, Clave],
-    (err, rows, fields) => {
-      if (!err) {
-        res.json({ Status: "Usuario saved" });
-      } else {
-        console.log(err);
-      }
-    }
-  );
-};
-
-const getAllUsers = (_, res) => {
-  const sql = "select * from usuarios";
   try {
-    mysqlconnection.query(sql, (err, rows, fields) => {
-      if (!err) {
-        rows.length >= 1
-          ? res.json(rows)
-          : res.json({ message: "No hay usuarios disponibles" });
-      } else {
-        return res.json({ msg: "Ocurrio un error", ok: false });
+    mysqlconnection.query(
+      sql,
+      [Nombre, Apellido, Email, Clave],
+      (err, rows, fields) => {
+        if (!err) {
+          return res.json({ msg: "Se guardo el usuario", ok: true });
+        } else {
+          return res.json({ msg: "Ocurrio un error", ok: false });
+        }
       }
-    });
+    );
   } catch (error) {
     return res.json({ msg: "Ocurrio un error", ok: false });
   }
+};
+
+const getAllUsers = (req, res) => {
+  let queryAsync = Promise.promisify(
+    mysqlconnection.query.bind(mysqlconnection)
+  );
+  let numRows;
+  let queryPagination;
+  const numPerPage = Number(req.query.npp) || 1;
+  const page = Number(req.query.page) || 1;
+  let numPages;
+  let skip = (page - 1) * numPerPage;
+  // Here we compute the LIMIT parameter for MySQL query
+  var limit = skip + "," + numPerPage;
+  queryAsync("SELECT count(*) as numRows FROM usuarios")
+    .then(function (results) {
+      numRows = results[0].numRows;
+      numPages = Math.ceil(numRows / numPerPage);
+    })
+    .then(() =>
+      queryAsync("SELECT * FROM usuarios ORDER BY ID DESC LIMIT " + limit)
+    )
+    .then(function (results) {
+      var responsePayload = {
+        users: results,
+      };
+      if (page <= numPages) {
+        responsePayload.pagination = {
+          totalItems: results.length,
+          totalPages: numPages,
+          currentPage: page,
+          prevPage: page <= 1 ? page : page - 1,
+          nextPage: page >= numPages ? page : page + 1,
+          take: numPerPage,
+        };
+      } else
+        responsePayload.pagination = {
+          err:
+            "queried page " +
+            page +
+            " is >= to maximum page number " +
+            numPages,
+        };
+      res.json(responsePayload);
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.json({ err: err });
+    });
 };
 
 const deleteUsers = (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const SitioRouter = require('./routes/Sitios.js');
-const UsersRouter = require('./routes/Users.js');
-const SitiosPRouter = require('./routes/SitioPopular.js');
+const SitioRouter = require("./routes/Sitios.js");
+const UsersRouter = require("./routes/Users.js");
+const SitiosPRouter = require("./routes/SitioPopular.js");
+const cors = require("cors");
 
 //Configuacion del server
-app.set('port',  process.env.PORT || 3080);
+app.set("port", process.env.PORT || 3080);
 
 app.use(express.json());
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 //middleware del server
 
 app.use(SitioRouter);
@@ -17,6 +18,7 @@ app.use(UsersRouter);
 app.use(SitiosPRouter);
 
 //lazamiento del server
-app.listen(app.get('port'),()=>{
-    console.log("this running in",app.get('port'));
-})
+
+app.listen(app.get("port"), () => {
+  console.log("this running in", app.get("port"));
+});
